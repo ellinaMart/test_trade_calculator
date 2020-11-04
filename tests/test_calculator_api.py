d3a3567ca4fbe9157d84d81e54482e342794f3fe
ellinaMart <ellina.martyn@gmail.com>
2020-11-04 17:21:48 +0300

    исправила описание

--- tests/test_calculator_api.py
+++ tests/test_calculator_api.py
@@ -8,7 +8,7 @@ from data.parameters import data_parameters
 
 @allure.feature('API TEST: Generate data')
 def test_generate_data(app):
-    with allure.step("Получаем список инструментов для формы standart и генерируем тестовые данные в файл data/parameters_mini.json"):
+    with allure.step("Генерируем тестовые данные в файл data/parameters_mini.json"):
         #app[1].open_calculator_page()
         #instruments = app[1].get_instruments_list()
         generate_data()
